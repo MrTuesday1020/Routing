55f077348356c96ae7b693118998e601b8ef1983
MrTuesday1020 <mrtuesday1020@gmail.com>
2017-10-13 16:07:45 +1100

    read file

--- RoutingPerformance.py
+++ RoutingPerformance.py
@@ -4,9 +4,9 @@
 # z5104857 Shi Xiaoyun
 
 # network type values: CIRCUIT or PACKET
-NETWORK_SCHEME = CIRCUIT
+NETWORK_SCHEME = "CIRCUIT"
 # routing scheme values: Shortest Hop Path (SHP), Shortest Delay Path (SDP) and Least Loaded Path (LLP)
-ROUTING_SCHEME = SHP
+ROUTING_SCHEME = "SHP"
 # a file contains the network topology specification
 TOPOLOGY_FILE = "topology.txt"
 # a file contains the virtual connection requests in the network
@@ -14,3 +14,13 @@ WORKLOAD_FILE = "workload.txt"
 # a positive integer value which show the number of packets per second which will be sent in each virtual connection.
 PACKET_RATE = 2
 
+# open and read TOPOLOGY_FILE
+with open(TOPOLOGY_FILE) as f:
+	routers = [line.strip().split(" ") for line in f]
+
+# open and reand WORKLOAD_FILE
+with open(WORKLOAD_FILE) as f:
+	requests = [line.strip().split(" ") for line in f]
+
+for router in routers:
+	print("insert source " + str(router[0]) + " and destination " + str(router[1]) + " with values " + str(router[2:4]))
